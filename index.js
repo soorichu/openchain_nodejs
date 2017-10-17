6dfd691b1aadfd5b874555dc157ff81877665765
soorichu <soorichu@gmail.com>
2017-10-17 20:38:33 +0900

    times

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/times', function(request, response) {
+	var result = '';
+	var times = process.env.TIMES || 5;
+	for(i=0; i<times; i++)
+		result += i + ' ';
+	response.send(result);
+});
