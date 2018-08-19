2cb30f8615d5485a21c0eacba9abb3f65a497772
soorichu <soorichu@gmail.com>
2018-08-19 13:24:33 +0900

    delete first python test file for fixing error

--- index.js
+++ index.js
@@ -57,21 +57,7 @@ app.get("/steem-json-parsing", function(request, response){
 	response.render('pages/steem-json-parsing');
 });
 
-app.get("/django_test", function(request, response){
-	var spawn = require('child_process').spawn,
-    py    = spawn('python', ['django_site/manage.py']),
-    data = [1,2,3,4,5,6,7,8,9],
-    dataString = '';
-    py.stdout.on('data', function(data){
-	  dataString += data.toString();
-	});
-	py.stdout.on('end', function(){
-	  console.log('Sum of numbers=',dataString);
-	});
-	py.stdin.write(JSON.stringify(data));
-	py.stdin.end();
-		response.render('pages/django_test')
-});
+
 
 app.get('/django_test2', function(resquest, response){
 	var exec = require('child_process').exec;
